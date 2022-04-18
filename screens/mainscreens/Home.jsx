a995db3db138e51a2c22c7c81742b59f84af2406
solomonsanthosh <solosanty2064@gmail.com>
2022-04-18 19:20:14 +0530

    S

--- screens/mainscreens/Home.jsx
+++ screens/mainscreens/Home.jsx
@@ -1,5 +1,4 @@
 import {
-<<<<<<< HEAD
   StyleSheet,
   Text,
   View,
@@ -16,38 +15,17 @@ import { aboutget, getEvents, getyoutube } from "../../axios/home.axios";
 import Swipercontainer from "../../components/swipercontainer";
 import Gridcont from "../../components/grid/grid";
 import Loading from './Loading'
-=======
-	StyleSheet,
-	Text,
-	View,
-	SafeAreaView,
-	ScrollView,
-	Image,
-	Dimensions,
-	StatusBar,
-	Animated
-} from 'react-native';
-import React, { useEffect, useRef, useState } from 'react';
-import { liveevent, yt } from '../../data/liveevent.data';
-import { aboutget, getEvents, getyoutube } from '../../axios/home.axios';
-import Swipercontainer from '../../components/swipercontainer';
-import Gridcont from '../../components/grid/grid';
-
->>>>>>> d1d207cf075adafb4cc7fb1f896f7c8c205034ca
 // import Swipercont from "../../components/swiper";
 // import {LinearGradient} from 'expo-linear-gradient';
 const { width, height } = Dimensions.get('window');
 
 const Home = ({ navigation }) => {
-<<<<<<< HEAD
-  setTimeout(() => {
-    setLoading(false);
-  }, 1000);
+  
   const scrollA = useRef(new Animated.Value(0)).current;
   const [about, setAbout] = useState([]);
   const [events, setEvents] = useState([]);
   const [youtube, setYoutube] = useState([]);
-  const [loading, setLoading] = useState(true);
+  
   const fetchHomeAbout = () =>
     aboutget()
       .then((res) => {
@@ -61,26 +39,6 @@ const Home = ({ navigation }) => {
     getEvents().then((res) => {
       setEvents(res.data);
     });
-=======
-	const scrollA = useRef(new Animated.Value(0)).current;
-	const [about, setAbout] = useState([]);
-	const [events, setEvents] = useState([]);
-	const [youtube, setYoutube] = useState([]);
-
-	const fetchHomeAbout = () =>
-		aboutget()
-			.then((res) => {
-				setAbout(res.data[0]);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	// fetchHomeAbout();
-	const fetchLiveEvents = () =>
-		getEvents().then((res) => {
-			setEvents(res.data);
-		});
->>>>>>> d1d207cf075adafb4cc7fb1f896f7c8c205034ca
 
 	const fetchYoutube = () =>
 		getyoutube().then((res) => {
@@ -99,10 +57,9 @@ const Home = ({ navigation }) => {
 		fetchYoutube();
 	}, [youtube]);
 
-<<<<<<< HEAD
   return (
     <>
-    {loading ? (<Loading/>) : (
+    
     <SafeAreaView style={styles.container}>
       <ScrollView
         style={styles.scrollView}
@@ -140,51 +97,8 @@ const Home = ({ navigation }) => {
           />
         </View>
       </ScrollView>
-    </SafeAreaView>)}</>
+    </SafeAreaView></>
   );
-=======
-	return (
-		<SafeAreaView style={styles.container}>
-			<ScrollView
-				style={styles.scrollView}
-				onScroll={Animated.event(
-					[{ nativeEvent: { contentOffset: { y: scrollA } } }],
-					{ useNativeDriver: false }
-				)}
-			>
-				<Animated.View style={styles.areaone(scrollA)}>
-					<Text style={styles.rajesh}>DR.RAJESH FERNANDO</Text>
-				</Animated.View>
-				<View style={styles.areacontainer}>
-					<View style={styles.areatwo}>
-						<View style={styles.aboutview}>
-							<Text style={styles.abouttitle}>About Able</Text>
-						</View>
-						<View style={styles.aboutcontent}>
-							<Text style={styles.abouttext}>{about.description}</Text>
-						</View>
-					</View>
-					<Swipercontainer liveevent={liveevent} title={'Live events'} />
-					<View style={styles.areatwo}>
-						<View style={styles.aboutview}>
-							<Text style={styles.abouttitle}>ABLE</Text>
-						</View>
-						<View>
-							<Gridcont />
-						</View>
-					</View>
-					<Swipercontainer liveevent={yt} title={'Youtube videos'} />
-					<StatusBar
-						barStyle='light-content'
-						hidden={false}
-						backgroundColor='#336DF5'
-						translucent={true}
-					/>
-				</View>
-			</ScrollView>
-		</SafeAreaView>
-	);
->>>>>>> d1d207cf075adafb4cc7fb1f896f7c8c205034ca
 };
 
 const styles = StyleSheet.create({
