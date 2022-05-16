21bfa1a98f9b8d0c13d80f02387eef104800b513
Prasanna <prasannavelmurugan@gmail.com>
2022-05-16 09:57:38 +0530

    rv

--- axios/meet.js
+++ axios/meet.js
@@ -1,58 +1,58 @@
-const axios = require('axios');
+const axios = require("axios");
 
 // const baseURL = "https://able-server.herokuapp.com";
-const baseURL = 'http://192.168.0.103:8080';
+const baseURL = "http://172.16.7.176:8080";
 
 export const addSlot = async (dates, values, meetLink, email) => {
-	// var json = JSON.stringify(values);
-	try {
-		return await axios
-			.post(`${baseURL}/meet/addslot`, {
-				time_slot: values,
-				date: dates,
-				meetLink: meetLink,
-				email: email
-			})
-			.then((res) => {
-				return res.data;
-			});
-	} catch (error) {
-		console.log(error);
-	}
+    // var json = JSON.stringify(values);
+    try {
+        return await axios
+            .post(`${baseURL}/meet/addslot`, {
+                time_slot: values,
+                date: dates,
+                meetLink: meetLink,
+                email: email,
+            })
+            .then((res) => {
+                return res.data;
+            });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const getSlot = async (date) => {
-	try {
-		console.log(date, 'p');
-		return await axios
-			.post(`${baseURL}/meet/getslot/`, {
-				date
-			})
-			.then((res) => {
-				console.log('====================================');
-				console.log(res);
-				console.log('====================================');
-				return res.data;
-			});
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        console.log(date, "p");
+        return await axios
+            .post(`${baseURL}/meet/getslot/`, {
+                date,
+            })
+            .then((res) => {
+                console.log("====================================");
+                console.log(res);
+                console.log("====================================");
+                return res.data;
+            });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export const sendMail = async (email, name, date, timeslot, slots) => {
-	try {
-		return await axios
-			.post(`${baseURL}/meet/mail/`, {
-				email,
-				name,
-				date,
-				timeslot,
-				slots
-			})
-			.then((res) => {
-				return res.data;
-			});
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        return await axios
+            .post(`${baseURL}/meet/mail/`, {
+                email,
+                name,
+                date,
+                timeslot,
+                slots,
+            })
+            .then((res) => {
+                return res.data;
+            });
+    } catch (error) {
+        console.log(error);
+    }
 };
